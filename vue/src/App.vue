342cb5d8aa28f3c26990737cd30dd5e4609d4ecd
c <ichlings@qq.com>
2024-07-17 20:00:20 +0800

    wasm

---
+++ vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+onMounted(() => {
+  console.log("mounted...");
+
+  
+});
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
