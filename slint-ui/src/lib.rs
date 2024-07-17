383e8d1caf9f825fb9a487a30026118d00068dfc
c <ichlings@qq.com>
2024-07-17 20:25:46 +0800

    wasm

---
+++ slint-ui/src/lib.rs
@@ -0,0 +1,42 @@
+//! wasm-pack build --dev --target web -d ../vue/src/wasm/slint
+
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+pub fn add(left: isize, right: isize) -> isize {
+    left + right
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    let win = MainWindow::new().unwrap();
+    win.run().unwrap();
+}
+
+slint::slint! {
+    import { VerticalBox, TextEdit, Button } from "std-widgets.slint";
+    export component MainWindow inherits Window {
+        title: "slint";
+        width: 500px;
+        height: 400px;
+
+        VerticalBox {
+            spacing: 20px;
+
+            te := TextEdit {
+
+            }
+
+            tx := Text{
+
+            }
+
+            Button {
+                text: "execute";
+                clicked => {
+                    tx.text = te.text;
+                }
+            }
+        }
+    }
+}
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,25 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import init, { add } from "@/wasm/slint";
 
 onMounted(() => {
   console.log("mounted...");
-
-  
+  init();
 });
+
+const test_add = () => {
+  alert(add(1, 2));
+}
 
 </script>
 
 <template>
-  
+  <div>
+    <canvas id="canvas"></canvas>
+    <div>
+      <button @click="test_add">add</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
